aaa38bef62d73571753ff5e5268f2aea8708f726
amos <amoswang@leadinfo.com.tw>
2026-05-10 01:41:49 +0800

    feat(copilot): D3 inline-extension composer + facet

---
+++ web/components/analyzer/cm6-inline/index.ts
@@ -0,0 +1,8 @@
+export {
+  inlineExtension,
+  inlineProvidersFacet,
+  setInlineState,
+  inlineField,
+} from "./inline-extension";
+export type { InlineProviders } from "./inline-extension";
+export type { InlineState } from "./inline-state";
